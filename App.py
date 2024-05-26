e1e353002868eb3f1812ffba6204a25feaef9b24
Vinge Nikolay <vn2756@gmail.com>
2024-05-26 21:59:45 +0400

    Environment.programRootPath

--- App.py
+++ App.py
@@ -1,7 +1,12 @@
+import os
+from game.lib.Environment import Environment
 from game.core.GameFactory import GameFactory
 
 
 class App:
+
+    def __init__(self):
+        Environment.programRootPath = os.path.dirname(__file__)
 
     def run(self):
         gameFactory = GameFactory()
