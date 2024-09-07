4e36b1cb44dd3f647c46a924699918e80d12d052
Vinge Nikolay <vn2756@gmail.com>
2024-09-07 21:01:39 +0400

    FloorVBOBuilder tess

--- game/render/level/FloorVBOBuilder.py
+++ game/render/level/FloorVBOBuilder.py
@@ -1,12 +1,41 @@
+from game.calc.Vector3Utils import Vector3Utils
+
+
 class FloorVBOBuilder:
 
     def build(self, floor, vboBuilder):
+        leftDirectionLength = floor.downLeft.getDirectionTo(floor.upLeft).getLength()
+        rightDirectionLength = floor.downRight.getDirectionTo(floor.upRight).getLength()
+        stepLength = 10
+        if leftDirectionLength > rightDirectionLength:
+            leftStepLength = stepLength
+            rightStepLength = stepLength * rightDirectionLength / leftDirectionLength
+        elif rightDirectionLength > leftDirectionLength:
+            leftStepLength = stepLength * leftDirectionLength / rightDirectionLength
+            rightStepLength = stepLength
+        else:
+            leftStepLength = stepLength
+            rightStepLength = stepLength
+
+        leftPoints = Vector3Utils.splitFromStartToEnd(floor.downLeft, floor.upLeft, leftStepLength)
+        rightPoints = Vector3Utils.splitFromStartToEnd(floor.downRight, floor.upRight, rightStepLength)
+        assert len(leftPoints) == len(rightPoints)
+
+        for i in range(1, len(leftPoints)):
+            downPoints = Vector3Utils.splitFromStartToEnd(leftPoints[i - 1], rightPoints[i - 1], stepLength)
+            stepLength = leftPoints[i].getDirectionTo(rightPoints[i]).getLength() / (len(downPoints) - 1)
+            upPoints = Vector3Utils.splitFromStartToEnd(leftPoints[i], rightPoints[i], stepLength)
+            assert len(downPoints) == len(upPoints)
+            for j in range(1, len(downPoints)):
+                self.addVertices(vboBuilder, floor, downPoints[j - 1], downPoints[j], upPoints[j - 1], upPoints[j])
+
+    def addVertices(self, vboBuilder, floor, downLeft, downRight, upLeft, upRight):
         vertexCount = vboBuilder.getVertexCount()
 
-        vboBuilder.addVertex(floor.downLeft)
-        vboBuilder.addVertex(floor.upLeft)
-        vboBuilder.addVertex(floor.upRight)
-        vboBuilder.addVertex(floor.downRight)
+        vboBuilder.addVertex(downLeft)
+        vboBuilder.addVertex(upLeft)
+        vboBuilder.addVertex(upRight)
+        vboBuilder.addVertex(downRight)
 
         vboBuilder.addNormal(floor.upNormal)
         vboBuilder.addNormal(floor.upNormal)
