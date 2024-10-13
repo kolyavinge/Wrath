bbddec7c80df43362915e37ffd8e7e0713c9e6b6
Vinge Nikolay <vn2756@gmail.com>
2024-10-13 21:09:15 +0400

    VBORenderer.renderList()

--- game/gl/VBORenderer.py
+++ game/gl/VBORenderer.py
@@ -8,6 +8,12 @@ class VBORenderer:
         glDrawElements(vbo.format, vbo.elementsCount, GL_UNSIGNED_INT, None)
         glBindVertexArray(0)
 
+    def renderList(self, vbos):
+        for vbo in vbos:
+            glBindVertexArray(vbo.vaoId)
+            glDrawElements(vbo.format, vbo.elementsCount, GL_UNSIGNED_INT, None)
+        glBindVertexArray(0)
+
 
 def makeVBORenderer(resolve):
     return VBORenderer()
