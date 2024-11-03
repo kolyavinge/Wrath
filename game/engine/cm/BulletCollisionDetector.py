73b578e5fcae10bb04c952fcc678bc91a3d2e627
Nikolay Vinge <vinge@megatec.ru>
2024-11-03 15:00:26 +0400

    BulletCollisionDetector new version

--- game/engine/cm/BulletCollisionDetector.py
+++ game/engine/cm/BulletCollisionDetector.py
@@ -1,18 +1,36 @@
+from game.engine.bsp.BSPTreeTraversal import BSPTreeTraversal
 from game.engine.cm.ConstructionCollisionDetector import ConstructionCollisionDetector
+from game.engine.GameData import GameData
 
 
 class BulletCollisionDetector:
 
-    def __init__(self, constructionCollisionDetector):
+    def __init__(self, gameData, constructionCollisionDetector, traversal):
+        self.gameData = gameData
         self.constructionCollisionDetector = constructionCollisionDetector
+        self.traversal = traversal
 
     def getConstructionCollisionResultOrNone(self, bullet):
-        result = self.constructionCollisionDetector.getConstructionCollisionResultOrNone(
-            bullet.currentLevelSegment, bullet.currentPosition, bullet.nextPosition
-        )
+        return self.getResultRec(bullet.currentLevelSegment, bullet.nextLevelSegment, bullet.currentPosition, bullet.nextPosition)
 
-        return result
+    def getResultRec(self, startSegment, endSegment, startPoint, endPoint):
+        if startSegment == endSegment:
+            return self.constructionCollisionDetector.getConstructionCollisionResultOrNone(startSegment, startPoint, endPoint)
+        else:
+            direction = startPoint.getDirectionTo(endPoint)
+            if direction.getLength() < 1.0:
+                return self.constructionCollisionDetector.getConstructionCollisionResultOrNone(
+                    startSegment, startPoint, endPoint
+                ) or self.constructionCollisionDetector.getConstructionCollisionResultOrNone(endSegment, startPoint, endPoint)
+            else:
+                direction.div(2)
+                middlePoint = startPoint.copy()
+                middlePoint.add(direction)
+                middleSegment = self.traversal.findLevelSegmentOrNone(self.gameData.level.collisionTree, middlePoint)
+                return self.getResultRec(startSegment, middleSegment, startPoint, middlePoint) or self.getResultRec(
+                    middleSegment, endSegment, middlePoint, endPoint
+                )
 
 
 def makeBulletCollisionDetector(resolver):
-    return BulletCollisionDetector(resolver.resolve(ConstructionCollisionDetector))
+    return BulletCollisionDetector(resolver.resolve(GameData), resolver.resolve(ConstructionCollisionDetector), resolver.resolve(BSPTreeTraversal))
