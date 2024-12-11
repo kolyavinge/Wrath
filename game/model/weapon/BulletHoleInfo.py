94d6916ae9417c3b64ff535326535fd3426a5230
Vinge Nikolay <vn2756@gmail.com>
2024-12-11 21:24:06 +0400

    bullet hole size

--- game/model/weapon/BulletHoleInfo.py
+++ game/model/weapon/BulletHoleInfo.py
@@ -8,8 +8,8 @@ class BulletHoleInfo:
         self.halfSize = halfSize
 
 
-BulletHoleInfo.smallHole = BulletHoleInfo(Material.bulletHole, 0.05)
-BulletHoleInfo.largeHole = BulletHoleInfo(Material.bulletHole, 0.1)
+BulletHoleInfo.smallHole = BulletHoleInfo(Material.bulletHole, 0.02)
+BulletHoleInfo.largeHole = BulletHoleInfo(Material.bulletHole, 0.05)
 BulletHoleInfo.plasmaHole = BulletHoleInfo(Material.blackHole, 0.1)
 BulletHoleInfo.explosionHole = BulletHoleInfo(Material.explosionHole, 1.0)
 BulletHoleInfo.railgunHole = BulletHoleInfo(Material.blackHole, 0.05)
