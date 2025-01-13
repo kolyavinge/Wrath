74be65faab564cca5c1b31b03c5cdc19352f0f43
Nikolay Vinge <vinge@megatec.ru>
2025-01-13 12:26:55 +0400

    EnemyRenderer update

--- game/render/person/EnemyRenderer.py
+++ game/render/person/EnemyRenderer.py
@@ -1,21 +1,18 @@
 from OpenGL.GL import *
 
-from game.engine.GameData import GameData
 from game.gl.VBORenderer import VBORenderer
 from game.render.person.EnemyRenderCollection import EnemyRenderCollection
 
 
 class EnemyRenderer:
 
-    def __init__(self, gameData, renderCollection, vboRenderer):
-        self.gameData = gameData
+    def __init__(self, renderCollection, vboRenderer):
         self.renderCollection = renderCollection
         self.vboRenderer = vboRenderer
 
     def render(self, shader, levelSegment):
         model = self.renderCollection.enemyModel
-        # for enemy in levelSegment.enemies:
-        for enemy in self.gameData.enemies:
+        for enemy in levelSegment.enemies:
             shader.setModelMatrix(enemy.getModelMatrix())
             for mesh in model.meshes:
                 shader.setMaterial(mesh.material)
@@ -24,4 +21,4 @@ class EnemyRenderer:
 
 
 def makeEnemyRenderer(resolver):
-    return EnemyRenderer(resolver.resolve(GameData), resolver.resolve(EnemyRenderCollection), resolver.resolve(VBORenderer))
+    return EnemyRenderer(resolver.resolve(EnemyRenderCollection), resolver.resolve(VBORenderer))
