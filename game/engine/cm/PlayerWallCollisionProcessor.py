669a1e3a5259fa2150ac1ce2092159c36aa60afa
Nikolay Vinge <vinge@megatec.ru>
2025-01-18 13:48:50 +0400

    PlayerWallCollisionProcessor for allPerson

--- game/engine/cm/PlayerWallCollisionProcessor.py
+++ game/engine/cm/PlayerWallCollisionProcessor.py
@@ -1,6 +1,6 @@
 from game.calc.Geometry import Geometry
 from game.calc.Vector3 import Vector3
-from game.engine.cm.PlayerWallCollisionDetector import PlayerWallCollisionDetector
+from game.engine.cm.PersonWallCollisionDetector import PersonWallCollisionDetector
 from game.engine.GameData import GameData
 from game.lib.Math import Math
 from game.model.Orientation import Orientation
@@ -8,37 +8,39 @@ from game.model.Orientation import Orientation
 
 class PlayerWallCollisionProcessor:
 
-    def __init__(self, gameData, playerWallCollisionDetector):
+    def __init__(self, gameData, personWallCollisionDetector):
         self.gameData = gameData
-        self.playerWallCollisionDetector = playerWallCollisionDetector
+        self.personWallCollisionDetector = personWallCollisionDetector
 
     def processCollisions(self):
-        collidedWalls = self.playerWallCollisionDetector.getCollidedWalls()
+        for person in self.gameData.allPerson:
+            self.processPersonCollisions(person)
+
+    def processPersonCollisions(self, person):
+        collidedWalls = self.personWallCollisionDetector.getCollidedWalls(person)
         if len(collidedWalls) == 0:
             return
 
-        self.processWall(collidedWalls[0])
+        self.processWall(person, collidedWalls[0])
 
-        player = self.gameData.player
         for i in range(1, len(collidedWalls)):
             wall = collidedWalls[i]
-            if self.playerWallCollisionDetector.hasCollision(player, wall):
-                self.processWall(wall)
+            if self.personWallCollisionDetector.hasCollision(person, wall):
+                self.processWall(person, wall)
 
-    def processWall(self, wall):
-        player = self.gameData.player
-        x, y = self.getPointOnLimitLine(wall, player.nextCenterPoint)
-        newNextPosition = Vector3(Math.round(x, 2), Math.round(y, 2), player.getZ())
-        player.moveNextPositionTo(newNextPosition)
+    def processWall(self, person, wall):
+        x, y = self.getPointOnLimitLine(wall, person.nextCenterPoint)
+        newNextPosition = Vector3(Math.round(x, 2), Math.round(y, 2), person.getZ())
+        person.moveNextPositionTo(newNextPosition)
 
-    def getPointOnLimitLine(self, wall, playerNextCenterPoint):
+    def getPointOnLimitLine(self, wall, personNextCenterPoint):
         if wall.orientation == Orientation.horizontal:
-            return (playerNextCenterPoint.x, wall.limitLine.startPoint.y)
+            return (personNextCenterPoint.x, wall.limitLine.startPoint.y)
         elif wall.orientation == Orientation.vertical:
-            return (wall.limitLine.startPoint.x, playerNextCenterPoint.y)
+            return (wall.limitLine.startPoint.x, personNextCenterPoint.y)
         elif wall.orientation == Orientation.diagonal:
-            playerDirection = wall.limitLine.startPoint.getDirectionTo(playerNextCenterPoint)
-            projected = Geometry.getProjectedVector(playerDirection, wall.limitLineDirection)
+            personDirection = wall.limitLine.startPoint.getDirectionTo(personNextCenterPoint)
+            projected = Geometry.getProjectedVector(personDirection, wall.limitLineDirection)
             projected.add(wall.limitLine.startPoint)
 
             return (projected.x, projected.y)
@@ -47,4 +49,4 @@ class PlayerWallCollisionProcessor:
 
 
 def makePlayerWallCollisionProcessor(resolver):
-    return PlayerWallCollisionProcessor(resolver.resolve(GameData), resolver.resolve(PlayerWallCollisionDetector))
+    return PlayerWallCollisionProcessor(resolver.resolve(GameData), resolver.resolve(PersonWallCollisionDetector))
