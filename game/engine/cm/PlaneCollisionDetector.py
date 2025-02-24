f3ac92e829baad365ebb38585e78fd517edba53b
Nikolay Vinge <vinge@megatec.ru>
2025-02-24 14:07:22 +0400

    PlaneCollisionDetector fix

--- game/engine/cm/PlaneCollisionDetector.py
+++ game/engine/cm/PlaneCollisionDetector.py
@@ -4,17 +4,30 @@ from game.lib.Numeric import Numeric
 class PlaneCollisionDetector:
 
     def getRectPlaneCollisionPointOrNone(self, startPoint, endPoint, rectPlane, eps):
-        collisionPoint = self.getPlaneCollisionPointOrNone(startPoint, endPoint, rectPlane.downLeft, rectPlane.normal)
+        collisionPoint = self.getPlaneCollisionPointOrNone(startPoint, endPoint, rectPlane.downLeft, rectPlane.normal, eps)
         if collisionPoint is not None and rectPlane.containsPoint(collisionPoint, eps):
             return collisionPoint
         else:
             return None
 
-    def getPlaneCollisionPointOrNone(self, startPoint, endPoint, basePoint, frontNormal):
+    def getPlaneCollisionPointOrNone(self, startPoint, endPoint, basePoint, frontNormal, eps):
+        startPointDotProduct = basePoint.getDirectionTo(startPoint).dotProduct(frontNormal)
+        endPointDotProduct = basePoint.getDirectionTo(endPoint).dotProduct(frontNormal)
+
+        # чтобы пересечение было, скалярные произведения должны иметь разные знаки
+        if startPointDotProduct * endPointDotProduct > 0:
+            return None
+
+        # для работы алгоритма startPoint должна находится перед лицевой стороной плоскости (dotProduct > 0)
+        if startPointDotProduct < 0:
+            # поворачивам плоскость на 180 градусов
+            frontNormal = frontNormal.copy()
+            frontNormal.mul(-1)
+
         middlePoint = startPoint.getMiddleTo(endPoint)
-        while startPoint.getLengthTo(endPoint) > 0.01:
+        while startPoint.getLengthTo(endPoint) > eps:
             dotProduct = basePoint.getDirectionTo(middlePoint).dotProduct(frontNormal)
-            if Numeric.floatEquals(dotProduct, 0, 0.1):
+            if Numeric.floatEquals(dotProduct, 0, eps):
                 break
             elif dotProduct > 0:
                 startPoint = middlePoint
