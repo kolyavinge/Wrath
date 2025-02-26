89a83c92e20d3426341da7665c9f9f282c896e25
Nikolay Vinge <vinge@megatec.ru>
2025-02-26 17:02:49 +0400

    TowersLevel

--- game/levels/TowersLevel.py
+++ game/levels/TowersLevel.py
@@ -65,12 +65,16 @@ class TowersLevel(Level):
         # light
         builder.buildLight(Vector3(1, 1, self.minZ + 2), 2)
 
+        self.addPowerupArea(PowerupArea(Vector3(50, 50, self.minZ), Vector3(50, 50, self.minZ), 40))
+
     def makeArea2(self, builder):
         # floor
         builder.buildRectSlab(Vector3(20, 150, self.minZ + 2), 80, 80, 1, Material.rock1, Material.rock1)
 
         # light
         builder.buildLight(Vector3(20, 150, self.minZ + 4), 2)
+
+        self.addPowerupArea(PowerupArea(Vector3(60, 190, self.minZ + 2), Vector3(60, 190, self.minZ + 2), 40))
 
     def makeArea3(self, builder):
         # floor
@@ -79,22 +83,41 @@ class TowersLevel(Level):
         # light
         builder.buildLight(Vector3(150, 50, self.minZ + 7), 2)
 
+        self.addPowerupArea(PowerupArea(Vector3(190, 90, self.minZ + 5), Vector3(190, 90, self.minZ + 5), 40))
+
     def makeJoinLines(self):
-        self.addJoinLine(LevelSegmentJoinLine(Vector3(100, 0, self.minZ + 2), Vector3(100, 100, self.minZ + 2)))
+        self.addJoinLine(LevelSegmentJoinLine(Vector3(120, 0, self.minZ + 2), Vector3(120, 100, self.minZ + 2)))
         self.addJoinLine(LevelSegmentJoinLine(Vector3(0, 100, self.minZ + 2), Vector3(90, 100, self.minZ + 2)))
 
     def getCollisionSplitPlanes(self):
         for sp in self.getVisibilitySplitPlanes():
             yield sp
 
+        # area1
         yield SplitPlane(Vector3(0, 0, self.minZ + 1), Vector3(0, 0, 1))
+        yield SplitPlane(Vector3(45, 10, self.minZ), Vector3(0, 1, 0))
+        yield SplitPlane(Vector3(45, 90, self.minZ), Vector3(0, -1, 0))
+        yield SplitPlane(Vector3(10, 45, self.minZ), Vector3(1, 0, 0))
+        yield SplitPlane(Vector3(90, 45, self.minZ), Vector3(-1, 0, 0))
+
+        # area2
+        yield SplitPlane(Vector3(50, 150, self.minZ + 2), Vector3(0, 1, 0))
+        yield SplitPlane(Vector3(50, 230, self.minZ + 2), Vector3(0, -1, 0))
+        yield SplitPlane(Vector3(20, 200, self.minZ + 2), Vector3(1, 0, 0))
+        yield SplitPlane(Vector3(100, 200, self.minZ + 2), Vector3(-1, 0, 0))
+
+        # area3
+        yield SplitPlane(Vector3(200, 50, self.minZ + 5), Vector3(0, 1, 0))
+        yield SplitPlane(Vector3(200, 130, self.minZ + 5), Vector3(0, -1, 0))
+        yield SplitPlane(Vector3(150, 100, self.minZ + 5), Vector3(1, 0, 0))
+        yield SplitPlane(Vector3(230, 100, self.minZ + 5), Vector3(-1, 0, 0))
 
     def getVisibilitySplitPlanes(self):
-        yield SplitPlane(Vector3(100, 0, 0), Vector3(1, 0, 0))
+        yield SplitPlane(Vector3(120, 0, 0), Vector3(1, 0, 0))
         yield SplitPlane(Vector3(0, 100, 0), Vector3(0, 1, 0))
 
     def getPlayerPosition(self):
-        return Vector3(10, 10, self.minZ)
+        return Vector3(11, 11, self.minZ)
 
     def getPlayerFrontNormal(self):
         return Vector3(1, 1, 0).getNormalized()
