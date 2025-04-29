6ba25537145748b5864414a8bc02a5f8be32e103
Nikolay Vinge <vinge@megatec.ru>
2025-04-29 15:33:49 +0400

    AIData vars for ObstacleAvoidanceLogic

--- game/engine/ai/ObstacleAvoidanceLogic.py
+++ game/engine/ai/ObstacleAvoidanceLogic.py
@@ -9,50 +9,47 @@ class ObstacleAvoidanceLogic:
 
     def __init__(self, collisionDetector):
         self.collisionDetector = collisionDetector
-        self.fieldViewRadians = Geometry.degreesToRadians(45.0)
-        self.viewDepth = 3.0
-        self.directionsCount = 8
-        self.radianStep = self.fieldViewRadians / self.directionsCount
         self.rand = Random()
 
     def getFrontNormalForNextStep(self, enemy):
         resultFrontNormal = Vector3()
 
-        isCurrentFrontNormalCorrect = False
+        aiData = enemy.aiData
+        isStraightDirectionCorrect = False
         direction = enemy.frontNormal.copy()
-        direction.setLength(self.viewDepth)
+        direction.setLength(aiData.checkCollisionLength)
         if not self.collisionDetector.anyCollisions(enemy, direction):
-            isCurrentFrontNormalCorrect = True
+            isStraightDirectionCorrect = True
             resultFrontNormal.add(direction)
 
         leftDirectionResult = Vector3()
         rightDirectionResult = Vector3()
         leftDirectionsCount = 0
         rightDirectionsCount = 0
-        radians = self.radianStep
-        for _ in range(0, self.directionsCount):
+        radians = aiData.checkCollisionRadianStep
+        for _ in range(0, aiData.checkCollisionDirectionsCount):
             direction = Geometry.rotatePoint(enemy.frontNormal, CommonConstants.zAxis, CommonConstants.axisOrigin, radians)
-            direction.setLength(self.viewDepth)
+            direction.setLength(aiData.checkCollisionLength)
             if not self.collisionDetector.anyCollisions(enemy, direction):
                 leftDirectionResult.add(direction)
                 leftDirectionsCount += 1
 
             direction = Geometry.rotatePoint(enemy.frontNormal, CommonConstants.zAxis, CommonConstants.axisOrigin, -radians)
-            direction.setLength(self.viewDepth)
+            direction.setLength(aiData.checkCollisionLength)
             if not self.collisionDetector.anyCollisions(enemy, direction):
                 rightDirectionResult.add(direction)
                 rightDirectionsCount += 1
 
-            radians += self.radianStep
+            radians += aiData.checkCollisionRadianStep
 
-        if isCurrentFrontNormalCorrect:
+        if isStraightDirectionCorrect:
             resultFrontNormal.add(leftDirectionResult)
             resultFrontNormal.add(rightDirectionResult)
         elif leftDirectionsCount > rightDirectionsCount:
             resultFrontNormal.add(leftDirectionResult)
         elif leftDirectionsCount < rightDirectionsCount:
             resultFrontNormal.add(rightDirectionResult)
-        else:
+        else:  # leftDirectionsCount == rightDirectionsCount
             if self.rand.getInt(-100, 100) % 2 == 0:
                 resultFrontNormal.add(leftDirectionResult)
             else:
@@ -65,6 +62,4 @@ class ObstacleAvoidanceLogic:
 
 
 def makeObstacleAvoidanceLogic(resolver):
-    return ObstacleAvoidanceLogic(
-        resolver.resolve(CollisionDetector),
-    )
+    return ObstacleAvoidanceLogic(resolver.resolve(CollisionDetector))
--- game/model/person/Enemy.py
+++ game/model/person/Enemy.py
@@ -1,8 +1,14 @@
+from game.calc.Geometry import Geometry
 from game.model.person.Person import Person
 
 
 class AIData:
-    pass
+
+    def __init__(self):
+        self.horizontalFieldViewRadians = Geometry.degreesToRadians(45.0)
+        self.checkCollisionLength = 3.0
+        self.checkCollisionDirectionsCount = 8
+        self.checkCollisionRadianStep = self.horizontalFieldViewRadians / self.checkCollisionDirectionsCount
 
 
 class Enemy(Person):
