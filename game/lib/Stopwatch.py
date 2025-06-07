b7ff7649c11581cc494ac5b4f3fa280972cd7d81
Nikolay Vinge <vinge@megatec.ru>
2025-06-07 13:20:35 +0400

    Stopwatch fix

--- game/lib/Stopwatch.py
+++ game/lib/Stopwatch.py
@@ -3,12 +3,15 @@ import time
 
 class Stopwatch:
 
+    def __init__(self):
+        self.elapsed = 0
+
     def start(self):
         self.startedAt = time.time()
 
     def stop(self):
         self.stopedAt = time.time()
-        self.elapsed = self.stopedAt - self.startedAt
+        self.elapsed += self.stopedAt - self.startedAt
 
     def printElapsed(self):
         print(f"{self.elapsed:.8}")
