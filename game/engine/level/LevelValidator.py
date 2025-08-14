c195e4fd7a6c194cb9587b0a07fd166b13b7d76f
Nikolay Vinge <vinge@megatec.ru>
2025-08-14 16:21:59 +0400

    remove LevelValidator.onlyOneLevelItemInVisibilitySegment()

--- game/engine/level/LevelValidator.py
+++ game/engine/level/LevelValidator.py
@@ -2,18 +2,4 @@ class LevelValidator:
 
     def validate(self, level, visibilityTree):
         level.validate()
-        allLevelSegments = visibilityTree.getAllLevelSegments()
-        self.onlyOneLevelItemInVisibilitySegment(allLevelSegments)
-
-    def onlyOneLevelItemInVisibilitySegment(self, allLevelSegments):
-        # TODO отключено
-        return
-        # если один обьект принадлежит нескольким сегментам, то он будет отрисовываться несколько раз
-        # из-за этого может неправильно обрабатываться свет
-        allLevelItems = set()
-        for levelSegment in allLevelSegments:
-            for levelItem in levelSegment.getAllVisibleStaticItems():
-                if levelItem not in allLevelItems:
-                    allLevelItems.add(levelItem)
-                else:
-                    raise Exception(f"Level item exists in more than one visibility segment.")
+        # allLevelSegments = visibilityTree.getAllLevelSegments()
