4139e909255e63f550e82317e1715da86d6a1546
Nikolay Vinge <vinge@megatec.ru>
2025-10-29 16:01:17 +0400

    convertListToLPLPChar()

--- game/lib/sys.py
+++ game/lib/sys.py
@@ -1,5 +1,17 @@
+import ctypes
+
 from termcolor import colored
 
 
 def warn(message):
     print(colored(f"Warning: {message}", "yellow"))
+
+
+def convertListToLPLPChar(lst):
+    c_char_p_array = (ctypes.c_char_p * len(lst))()
+    for i, s in enumerate(lst):
+        c_char_p_array[i] = s.encode("utf-8")
+    LP_LP_c_char = ctypes.POINTER(ctypes.POINTER(ctypes.c_char))
+    result = ctypes.cast(c_char_p_array, LP_LP_c_char)
+
+    return result
