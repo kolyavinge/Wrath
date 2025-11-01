4c0b0385324b83e198e2880f81f5160ee18e7114
Nikolay Vinge <vinge@megatec.ru>
2025-11-01 13:08:29 +0400

    ShadowedObjectFramebuffer check create status

--- game/render/main/ShadowedObjectFramebuffer.py
+++ game/render/main/ShadowedObjectFramebuffer.py
@@ -47,5 +47,8 @@ class ShadowedObjectFramebuffer:
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, self.stencilMaskTexture, 0)
         glDrawBuffers(3, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2])
 
+        if glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE:
+            raise Exception("ShadowedObjectFramebuffer create error.")
+
         glBindTexture(GL_TEXTURE_2D, 0)
         glBindFramebuffer(GL_FRAMEBUFFER, GL_DEFAULT_FRAMEBUFFER_ID)
