6cf78ff3a9dbea8a570dd98b416f06f6146ddc55
Nikolay Vinge <vinge@megatec.ru>
2025-11-17 09:13:58 +0400

    projectStatistic.py upd

--- game/tools/projectStatistic.py
+++ game/tools/projectStatistic.py
@@ -1,8 +1,14 @@
+# print some project statistics
+
 import os
+
+bigFileLinesCount = 200
 
 pyFilesCount = 0
 linesCount = 0
 classesCount = 0
+bigFilesCount = 0
+bigFileNames = []
 
 
 def isCodeLine(line):
@@ -19,29 +25,38 @@ def analyzeFile(filePath):
     global pyFilesCount
     global linesCount
     global classesCount
+    global bigFilesCount
     _, extension = os.path.splitext(filePath)
     if extension == ".py":
         pyFilesCount += 1
         with open(filePath, "r") as file:
             lines = file.readlines()
+            codeLines = 0
             for line in lines:
                 if isCodeLine(line):
-                    linesCount += 1
+                    codeLines += 1
                     if line.startswith("class "):
                         classesCount += 1
+            linesCount += codeLines
+            if codeLines >= bigFileLinesCount:
+                bigFilesCount += 1
+                bigFileNames.append(filePath)
 
 
-def analyzeDir(parentPath):
+def analyzeProjectDir(parentPath):
     for childName in os.listdir(parentPath):
         childPath = os.path.join(parentPath, childName)
         if os.path.isfile(childPath):
             analyzeFile(childPath)
         else:
-            analyzeDir(childPath)
+            analyzeProjectDir(childPath)
 
 
-analyzeDir("D:\\Projects\\Wrath\\game")
+analyzeProjectDir("D:\\Projects\\Wrath\\game")
 
 print(f"python files count: {pyFilesCount}")
 print(f"python files lines count: {linesCount:,d}")
 print(f"classes count: {classesCount}")
+print(f"big files count: {bigFilesCount}")
+if len(bigFileNames) > 0:
+    print(bigFileNames)
