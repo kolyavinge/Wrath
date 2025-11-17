d6e599ca98a396cee61cdc55563c17252ae8a231
Nikolay Vinge <vinge@megatec.ru>
2025-11-17 20:20:53 +0400

    ShaderCollection alphabet sort

--- game/render/common/ShaderCollection.py
+++ game/render/common/ShaderCollection.py
@@ -13,6 +13,18 @@ class ShaderCollection:
 
         self.common = self.shaderCompiler.compile(path + "common.glsl", ShaderType.vertex)
 
+        self.blurVertex = self.shaderCompiler.compile(path + "blur.vert", ShaderType.vertex)
+        self.blurFragment = self.shaderCompiler.compile(path + "blur.frag", ShaderType.fragment)
+
+        self.crosshairVertex = self.shaderCompiler.compile(path + "crosshair.vert", ShaderType.vertex)
+        self.crosshairFragment = self.shaderCompiler.compile(path + "crosshair.frag", ShaderType.fragment)
+
+        self.launcherBulletTraceVertex = self.shaderCompiler.compile(path + "launcherBulletTrace.vert", ShaderType.vertex)
+        self.launcherBulletTraceFragment = self.shaderCompiler.compile(path + "launcherBulletTrace.frag", ShaderType.fragment)
+
+        self.mainSceneComposeVertex = self.shaderCompiler.compile(path + "mainSceneCompose.vert", ShaderType.vertex)
+        self.mainSceneComposeFragment = self.shaderCompiler.compile(path + "mainSceneCompose.frag", ShaderType.fragment)
+
         self.mainSceneLightComponentsVertex = self.shaderCompiler.compile(path + "mainSceneLightComponents.vert", ShaderType.vertex)
         self.mainSceneLightComponentsFragment = self.shaderCompiler.compile(path + "mainSceneLightComponents.frag", ShaderType.fragment)
 
@@ -23,35 +35,23 @@ class ShaderCollection:
         self.mainSceneStencilMaskVertex = self.shaderCompiler.compile(path + "mainSceneStencilMask.vert", ShaderType.vertex)
         self.mainSceneStencilMaskFragment = self.shaderCompiler.compile(path + "mainSceneStencilMask.frag", ShaderType.fragment)
 
-        self.mainSceneComposeVertex = self.shaderCompiler.compile(path + "mainSceneCompose.vert", ShaderType.vertex)
-        self.mainSceneComposeFragment = self.shaderCompiler.compile(path + "mainSceneCompose.frag", ShaderType.fragment)
-
-        self.crosshairVertex = self.shaderCompiler.compile(path + "crosshair.vert", ShaderType.vertex)
-        self.crosshairFragment = self.shaderCompiler.compile(path + "crosshair.frag", ShaderType.fragment)
+        self.meshExtVertex = self.shaderCompiler.compile(path + "meshExt.vert", ShaderType.vertex)
+        self.meshExtFragment = self.shaderCompiler.compile(path + "meshExt.frag", ShaderType.fragment)
 
         self.meshVertex = self.shaderCompiler.compile(path + "mesh.vert", ShaderType.vertex)
         self.meshFragment = self.shaderCompiler.compile(path + "mesh.frag", ShaderType.fragment)
 
-        self.meshExtVertex = self.shaderCompiler.compile(path + "meshExt.vert", ShaderType.vertex)
-        self.meshExtFragment = self.shaderCompiler.compile(path + "meshExt.frag", ShaderType.fragment)
+        self.plainColorVertex = self.shaderCompiler.compile(path + "plainColor.vert", ShaderType.vertex)
+        self.plainColorFragment = self.shaderCompiler.compile(path + "plainColor.frag", ShaderType.fragment)
 
-        self.shineCircleVertex = self.shaderCompiler.compile(path + "shineCircle.vert", ShaderType.vertex)
-        self.shineCircleFragment = self.shaderCompiler.compile(path + "shineCircle.frag", ShaderType.fragment)
+        self.plasmaExplosionVertex = self.shaderCompiler.compile(path + "plasmaExplosion.vert", ShaderType.vertex)
+        self.plasmaExplosionFragment = self.shaderCompiler.compile(path + "plasmaExplosion.frag", ShaderType.fragment)
 
         self.rayVertex = self.shaderCompiler.compile(path + "ray.vert", ShaderType.vertex)
         self.rayFragment = self.shaderCompiler.compile(path + "ray.frag", ShaderType.fragment)
 
-        self.plainColorVertex = self.shaderCompiler.compile(path + "plainColor.vert", ShaderType.vertex)
-        self.plainColorFragment = self.shaderCompiler.compile(path + "plainColor.frag", ShaderType.fragment)
+        self.shineCircleVertex = self.shaderCompiler.compile(path + "shineCircle.vert", ShaderType.vertex)
+        self.shineCircleFragment = self.shaderCompiler.compile(path + "shineCircle.frag", ShaderType.fragment)
 
         self.vignetteVertex = self.shaderCompiler.compile(path + "vignette.vert", ShaderType.vertex)
         self.vignetteFragment = self.shaderCompiler.compile(path + "vignette.frag", ShaderType.fragment)
-
-        self.launcherBulletTraceVertex = self.shaderCompiler.compile(path + "launcherBulletTrace.vert", ShaderType.vertex)
-        self.launcherBulletTraceFragment = self.shaderCompiler.compile(path + "launcherBulletTrace.frag", ShaderType.fragment)
-
-        self.blurVertex = self.shaderCompiler.compile(path + "blur.vert", ShaderType.vertex)
-        self.blurFragment = self.shaderCompiler.compile(path + "blur.frag", ShaderType.fragment)
-
-        self.plasmaExplosionVertex = self.shaderCompiler.compile(path + "plasmaExplosion.vert", ShaderType.vertex)
-        self.plasmaExplosionFragment = self.shaderCompiler.compile(path + "plasmaExplosion.frag", ShaderType.fragment)
