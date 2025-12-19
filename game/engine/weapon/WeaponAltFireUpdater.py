097c595033ba3c6cbe2020a8b84b9daf11173de0
Nikolay Vinge <vinge@megatec.ru>
2025-12-19 17:24:33 +0400

    WeaponFireUpdater and WeaponAltFireUpdater fire/altFire check

--- game/engine/weapon/WeaponAltFireUpdater.py
+++ game/engine/weapon/WeaponAltFireUpdater.py
@@ -2,6 +2,7 @@ from game.engine.GameData import GameData
 from game.engine.weapon.LauncherAltFireLogic import LauncherAltFireLogic
 from game.engine.weapon.RailgunAltFireLogic import RailgunAltFireLogic
 from game.engine.weapon.SniperAltFireLogic import SniperAltFireLogic
+from game.model.person.PersonInputData import FireState
 from game.model.weapon.Launcher import Launcher
 from game.model.weapon.Railgun import Railgun
 from game.model.weapon.Sniper import Sniper
@@ -24,7 +25,8 @@ class WeaponAltFireUpdater:
 
     def update(self):
         for person, inputData in self.gameData.allPersonInputData.items():
-            personItems = self.gameData.allPersonItems[person]
-            weaponType = personItems.getCurrentWeaponType()
-            if weaponType in self.altFireLogic:
-                self.altFireLogic[weaponType].apply(person, personItems, inputData)
+            if not inputData.fire and inputData.altFireState != FireState.deactive:
+                personItems = self.gameData.allPersonItems[person]
+                weaponType = personItems.getCurrentWeaponType()
+                if weaponType in self.altFireLogic:
+                    self.altFireLogic[weaponType].apply(person, personItems, inputData)
--- game/engine/weapon/WeaponFireUpdater.py
+++ game/engine/weapon/WeaponFireUpdater.py
@@ -1,5 +1,6 @@
 from game.engine.GameData import GameData
 from game.engine.weapon.WeaponFireLogic import WeaponFireLogic
+from game.model.person.PersonInputData import FireState
 
 
 class WeaponFireUpdater:
@@ -15,6 +16,6 @@ class WeaponFireUpdater:
     def update(self):
         for person, inputData in self.gameData.allPersonInputData.items():
             personItems = self.gameData.allPersonItems[person]
-            personItems.currentWeapon.isFiring = False
-            if inputData.fire:
+            personItems.currentWeapon.isFiring = False  # reset firing before process fire
+            if inputData.fire and inputData.altFireState == FireState.deactive:
                 self.weaponFireLogic.fire(person, personItems)
