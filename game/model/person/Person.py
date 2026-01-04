01bdba1674abddfbd44cb41daa43cc86565beaf0
Nikolay Vinge <vinge@megatec.ru>
2026-01-04 14:12:50 +0400

    move getAlphaForLifeCycle() from Enemy to Person

--- game/model/person/Person.py
+++ game/model/person/Person.py
@@ -117,6 +117,18 @@ class Person:
     def isParalyzed(self):
         return not self.paralyzeDelay.isExpired()
 
+    def getAlphaForLifeCycle(self):
+        if self.lifeCycle == LifeCycle.alive:
+            return 1.0
+        if self.lifeCycle == LifeCycle.dead:
+            return self.lifeCycleDelay.value / self.lifeCycleDelay.initValue
+        if self.lifeCycle == LifeCycle.respawnDelay:
+            return 0.0
+        if self.lifeCycle == LifeCycle.respawn:
+            return 1.0 - self.lifeCycleDelay.value / self.lifeCycleDelay.initValue
+
+        raise Exception("Wrong LifeCycle value.")
+
     def getModelMatrix(self):
         return (
             TransformMatrix4Builder()
